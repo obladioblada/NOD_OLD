9a2164270ef033898b92b287148f36e9d5952d25
Ajeje Brazorf <peppemini@gmail.com>
2016-05-01 14:11:01 +0200

    Prima versione del nod

---
+++ nod/js/app.js
@@ -0,0 +1,107 @@
+'use strict';
+angular.module('mainApp',['ngAudio','firebase','ngSanitize'])
+    .controller('mainCtrl', function ($scope, ngAudio,  $firebaseArray){
+        $scope.userid="ballalsfbas";
+        $scope.myuserid="Gary";
+        $scope.myUser={
+            song:'',
+            time: '',
+            isPlaying: false
+        };
+        $scope.min=false;
+        $scope.numero=0;
+        $scope.songs=["track00","track01","track02","track03","track04"];
+        $scope.currentSongIndex;
+
+        $scope.determinaUser=function(){
+            angular.forEach( $scope.messages, function(user) {
+                if(user.$id==$scope.myuserid){
+                    $scope.myUser=user;
+                }
+            })
+        };
+
+        $scope.chooseSong= function($i){
+            $scope.audio = ngAudio.load("audio/"+$scope.songs[$i]+".mp3");
+            $scope.audio.loop=false;
+            $scope.currentSongIndex=$i;
+            $scope.audio.play();
+        };
+        $scope.percentage=0;
+        $scope.chooseSong(0);
+        $scope.checkIfPlaying=function(){
+            if(!$scope.audio.paused) {
+                $scope.audio.play();
+            }
+        };
+        $scope.updateBar=function(){
+            $scope.tot=$scope.audio.currentTime+$scope.audio.remaining;
+            $scope.percentage=$scope.audio.currentTime*100/$scope.tot;
+            $scope.percentage=100-$scope.percentage;
+            $scope.style=".player-slider::before { top: 0; left: 0; bottom: 0; right: "+$scope.percentage+"%; border-radius: 4px; background: #222; } .player-slider::after { opacity: 1; right: "+$scope.percentage+"%; width: 10px; height: 10px; top: -2.5px; background: #fff; border-radius: 50%; box-shadow: 0 0 1px rgba(0,0,0,0.2); }";
+        };
+        $scope.$watch('audio.currentTime',function(){
+            $scope.updateBar();
+            if($scope.audio.remaining<1){
+                $scope.nextSong();
+            }
+            $scope.sendToFB();
+         });
+        $scope.playOrPause= function(){
+            if($scope.audio.paused){
+                $scope.audio.play();
+            }else{
+                $scope.audio.pause();
+            }
+
+        };
+        $scope.nextSong= function (){
+            $scope.currentSongIndex++;
+            if($scope.currentSongIndex==$scope.songs.length){
+                $scope.currentSongIndex=0;
+            }
+            $scope.audio.stop();
+            $scope.chooseSong($scope.currentSongIndex);
+        };
+
+        $scope.prevSong= function (){
+            $scope.currentSongIndex--;
+            if($scope.currentSongIndex<0){
+                $scope.currentSongIndex=$scope.songs.length-1;
+            }$scope.audio.stop();
+            $scope.chooseSong($scope.currentSongIndex);
+        };
+
+        $scope.sendToFB=function () {
+            if ($scope.myUser != undefined && $scope.myUser != null) {
+                $scope.myUser.song = $scope.currentSongIndex;
+                $scope.myUser.time = $scope.audio.currentTime;
+                if($scope.myUser.time==undefined) $scope.myUser.time=null;
+                $scope.myUser.isPlaying=!$scope.audio.paused;
+                $scope.messages.$save($scope.myUser);
+            }
+        };
+
+
+        var ref = new Firebase("https://nod-music.firebaseio.com/users");
+        $scope.messages = $firebaseArray(ref);
+
+        $scope.messages.$loaded()
+            .then(function(){
+                angular.forEach( $scope.messages, function(user) {
+                    if(user.$id==$scope.myuserid){
+                        $scope.myUser=user;
+                    }
+                })
+            });
+
+        $scope.listenTo=function($song,$time){
+            $scope.audio.stop();
+            $scope.audio = ngAudio.load("audio/"+$scope.songs[$song]+".mp3");
+            $scope.currentSongIndex=$song;
+            $scope.audio.setCurrentTime($time);
+            $scope.audio.play();
+        };
+
+
+    });

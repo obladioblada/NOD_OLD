9e096f78fcc8d355937c1d29aee6c762f6386e80
Ajeje Brazorf <peppemini@gmail.com>
2016-06-09 15:56:48 +0200

    broswerkey project number

--- nod/sw.js
+++ nod/sw.js
@@ -7,6 +7,7 @@ self.addEventListener('activate', function(event) {
     console.log('Activated', event);
 });
 self.addEventListener('push', function(event) {
-    console.log('Push message received', event);
-    // TODO
+    console.log('Received a push message', event);
+// broswerKey AIzaSyA2osVlaB52G_k5Rp1D4VP8QG0NrhnRAm8
+//project number 538074622362
 });
